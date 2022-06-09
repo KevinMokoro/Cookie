75ddd7cc7379c584c49678d9fa8958a26d090a02
KevinMokoro <klosvoke1@gmail.com>
2022-06-10 01:42:45 +0300

    This will add Constants required by the API.

---
+++ app/src/main/java/com/moringaschool/cookie/Constants.java
@@ -0,0 +1,8 @@
+package com.moringaschool.cookie;
+
+public class Constants {
+    public static final String EDAMAM_BASE_URL = "https://api.edamam.com";
+    public  static  final  String SEARCH_TYPE ="public";
+    public static final String EDAMAM_ID = BuildConfig.EDAMAM_ID;
+    public static final String EDAMAM_API_KEY = BuildConfig.EDAMAM_API_KEY;
+}

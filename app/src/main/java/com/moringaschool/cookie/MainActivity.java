29e6d63ed38ab51f3825c2e971455a33f0200988
KevinMokoro <klosvoke1@gmail.com>
2022-06-09 03:16:27 +0300

    This will implement an interface View.OnClicklistener in the main activity.

--- app/src/main/java/com/moringaschool/cookie/MainActivity.java
+++ app/src/main/java/com/moringaschool/cookie/MainActivity.java
@@ -3,13 +3,14 @@ package com.moringaschool.cookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.findRecipes) Button mFindRecipes;
     @BindView(R.id.ingredientEditText) EditText mIngredientEditText;
 
@@ -18,5 +19,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        mFindRecipes.setOnClickListener(this);
     }
 }

d81e079013e3d23ed98f088bd6d4bd0ce896fd78
KevinMokoro <klosvoke1@gmail.com>
2022-06-09 03:04:30 +0300

    This will bind all the views to be used in business in respective areas.

--- app/src/main/java/com/moringaschool/cookie/MainActivity.java
+++ app/src/main/java/com/moringaschool/cookie/MainActivity.java
@@ -3,12 +3,20 @@ package com.moringaschool.cookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.findRecipes) Button mFindRecipes;
+    @BindView(R.id.ingredientEditText) EditText mIngredientEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/com/moringaschool/cookie/RecipesActivity.java
+++ app/src/main/java/com/moringaschool/cookie/RecipesActivity.java
@@ -3,12 +3,20 @@ package com.moringaschool.cookie;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class RecipesActivity extends AppCompatActivity {
+    @BindView(R.id.recipesTextView) TextView mRecipesTextView;
+    @BindView(R.id.listView) ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
+        ButterKnife.bind(this);
     }
 }

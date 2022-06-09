ff2b02f4afec37804b7a8b6015d2d244f68a66a4
KevinMokoro <klosvoke1@gmail.com>
2022-06-09 05:15:16 +0300

    This will finish the display of recipes, remove the AppBar from homescreen and initialize an empty about page.

--- app/src/main/java/com/moringaschool/cookie/RecipesActivity.java
+++ app/src/main/java/com/moringaschool/cookie/RecipesActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,12 +16,28 @@ import butterknife.ButterKnife;
 public class RecipesActivity extends AppCompatActivity {
     @BindView(R.id.recipesTextView) TextView mRecipesTextView;
     @BindView(R.id.listView) ListView mListView;
+    String [] recipes = new String[] {"ugali", "nyama", "mala", "githeri", "mchele", "omena",
+    "mayai", "chai", "maziwa", "wali", "pizza", "yoo", "juice", "matoke"};
+
+    String [] sources = new String[] {"kin", "els", "per", "elna", "JAK", "chr", "sim",
+    "sab", "sai", "GEO", "ole", "omu", "tankua", "wakesho"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipes);
         ButterKnife.bind(this);
+
+        MyRecipesAdapter adapter = new MyRecipesAdapter(this, android.R.layout.simple_list_item_1, recipes, sources);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?>parent, View view, int position, long l) {
+                String recipe = ((TextView)view).getText().toString();
+                Toast.makeText(RecipesActivity.this, recipe, Toast.LENGTH_LONG).show();
+            }
+        });
 
         Intent intent = getIntent();
         String ingredient = intent.getStringExtra("ingredient");

cd96f4692d961d01b3c92daf3d26e4c385c98f44
KevinMokoro <klosvoke1@gmail.com>
2022-06-10 12:40:27 +0300

    This will add Recipelist adapter.

---
+++ app/src/main/java/com/moringaschool/cookie/adapters/RecipeListAdapter.java
@@ -0,0 +1,67 @@
+package com.moringaschool.cookie.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.cookie.R;
+import com.moringaschool.cookie.models.Hit;
+import com.moringaschool.cookie.models.Recipe;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class RecipeListAdapter extends RecyclerView.Adapter<RecipeListAdapter.RecipeViewHolder> {
+    private List<Hit> mRecipes;
+    private Context mContext;
+
+    public RecipeListAdapter(Context context, List<Hit> recipes) {
+        mContext = context;
+        mRecipes = recipes;
+    }
+
+    @Override
+    public RecipeListAdapter.RecipeViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+        RecipeViewHolder viewHolder = new RecipeViewHolder(view);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecipeListAdapter.RecipeViewHolder holder, int position) {
+        holder.bindRecipe(mRecipes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mRecipes.size();
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.recipeImageView) ImageView mRecipeImageView;
+        @BindView(R.id.recipeNameTextView) TextView mNameTextView;
+
+
+        private Context mContext;
+
+        public RecipeViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            mContext = itemView.getContext();
+        }
+
+        public void bindRecipe(Hit recipe) {
+            Picasso.get().load(recipe.getRecipe().getImage()).into(mRecipeImageView);
+            mNameTextView.setText(recipe.getRecipe().getLabel());
+        }
+    }
+}
+

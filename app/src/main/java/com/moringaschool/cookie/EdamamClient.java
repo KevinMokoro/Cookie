2cde0819f9b5f3e3192745e11f7b42fe516adb8f
KevinMokoro <klosvoke1@gmail.com>
2022-06-10 03:58:34 +0300

    This will add an EdamamClient.

---
+++ app/src/main/java/com/moringaschool/cookie/EdamamClient.java
@@ -0,0 +1,38 @@
+package com.moringaschool.cookie;
+
+import static com.moringaschool.cookie.Constants.EDAMAM_BASE_URL;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class EdamamClient {
+    private  static Retrofit retrofit = null;
+
+    public  static  EdamamApi getClient(){
+        if (retrofit == null){
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .addInterceptor(new Interceptor() {
+                        @Override
+                        public Response intercept(Chain chain) throws IOException {
+                            Request newRequest = chain.request().newBuilder()
+                                    .build();
+                            return chain.proceed(newRequest);
+                        }
+                    })
+                    .build();
+
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(EDAMAM_BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return  retrofit.create(EdamamApi.class);
+    }
+}

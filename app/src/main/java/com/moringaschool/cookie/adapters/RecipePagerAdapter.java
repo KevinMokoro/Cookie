45b8b6f61d3a6374ec7fe8ad20ac9d79063d0abd
KevinMokoro <klosvoke1@gmail.com>
2022-06-12 13:00:07 +0300

    This will pageradapter to connect to our view.

---
+++ app/src/main/java/com/moringaschool/cookie/adapters/RecipePagerAdapter.java
@@ -0,0 +1,37 @@
+package com.moringaschool.cookie.adapters;
+
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.moringaschool.cookie.models.Hit;
+import com.moringaschool.cookie.ui.RecipesDetailFragment;
+
+import java.util.List;
+
+public class RecipePagerAdapter extends FragmentPagerAdapter {
+    private List<Hit> mRecipes;
+
+    public RecipePagerAdapter (@NonNull FragmentManager fm, int behavior, List<Hit> recipes) {
+        super(fm, behavior);
+        mRecipes = recipes;
+    }
+
+    @Override
+    public RecipesDetailFragment getItem(int position) {
+        return RecipesDetailFragment.newInstance(mRecipes.get(position));
+    }
+
+    @Override
+    public int getCount (){
+        return mRecipes.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return mRecipes.get(position).getRecipe().getLabel();
+    }
+
+
+}

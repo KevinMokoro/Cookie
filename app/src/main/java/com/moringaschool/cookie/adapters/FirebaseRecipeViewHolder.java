66bec1bb4bf5a3eee54d0ded8474aa9dbef4deea
KevinMokoro <klosvoke1@gmail.com>
2022-06-15 01:25:52 +0300

    This will create functionality to display saved items/recipes.

---
+++ app/src/main/java/com/moringaschool/cookie/adapters/FirebaseRecipeViewHolder.java
@@ -0,0 +1,75 @@
+package com.moringaschool.cookie.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.moringaschool.cookie.Constants;
+import com.moringaschool.cookie.R;
+import com.moringaschool.cookie.models.Hit;
+import com.moringaschool.cookie.models.Recipe;
+import com.moringaschool.cookie.ui.RecipesDetailActivity;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+public class FirebaseRecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+    View mView;
+    Context mContext;
+
+    public FirebaseRecipeViewHolder(View itemView) {
+        super(itemView);
+        mView = itemView;
+        mContext = itemView.getContext();
+        itemView.setOnClickListener(this);
+    }
+
+    public void bindRecipe(Hit recipe) {
+        ImageView recipeImageView = (ImageView) mView.findViewById(R.id.recipeImageView);
+        TextView nameTextView = (TextView) mView.findViewById(R.id.recipeNameTextView);
+
+        Picasso.get().load(recipe.getRecipe().getImage()).into(recipeImageView);
+
+        nameTextView.setText(recipe.getRecipe().getLabel());
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        final ArrayList<Hit> recipes = new ArrayList<>();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES);
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    recipes.add(snapshot.getValue(Hit.class));
+                }
+
+                int itemPosition = getLayoutPosition();
+
+                Intent intent = new Intent(mContext, RecipesDetailActivity.class);
+                intent.putExtra("position", itemPosition + "");
+                intent.putExtra("recipes", Parcels.wrap(recipes));
+
+                mContext.startActivity(intent);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/moringaschool/cookie/ui/SavedRecipeListActivity.java
@@ -0,0 +1,94 @@
+package com.moringaschool.cookie.ui;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.moringaschool.cookie.Constants;
+import com.moringaschool.cookie.R;
+import com.moringaschool.cookie.adapters.FirebaseRecipeViewHolder;
+import com.moringaschool.cookie.models.Hit;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SavedRecipeListActivity extends AppCompatActivity {
+
+    private DatabaseReference mRecipeReference;
+    private FirebaseRecyclerAdapter<Hit, FirebaseRecipeViewHolder> mFirebaseAdapter;
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+        ButterKnife.bind(this);
+
+        mRecipeReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RECIPES);
+        setUpFirebaseAdapter();
+        hideProgressBar();
+        showRestaurants();
+
+    }
+
+    private void setUpFirebaseAdapter(){
+        FirebaseRecyclerOptions<Hit> options =
+                new FirebaseRecyclerOptions.Builder<Hit>()
+                        .setQuery(mRecipeReference, Hit.class)
+                        .build();
+
+        mFirebaseAdapter = new FirebaseRecyclerAdapter<Hit, FirebaseRecipeViewHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull FirebaseRecipeViewHolder FirebaseRecipeViewHolder, int position, @NonNull Hit recipe) {
+                FirebaseRecipeViewHolder.bindRecipe(recipe);
+            }
+
+            @NonNull
+            @Override
+            public FirebaseRecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recipe_list_item, parent, false);
+                return new FirebaseRecipeViewHolder(view);
+            }
+        };
+
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(mFirebaseAdapter);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mFirebaseAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mFirebaseAdapter!= null) {
+            mFirebaseAdapter.stopListening();
+        }
+    }
+
+    private void showRestaurants() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+
+}

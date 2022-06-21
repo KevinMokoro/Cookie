185e989c3afeaf89885b61b79960f9ac7205ef30
KevinMokoro <klosvoke1@gmail.com>
2022-06-21 05:48:52 +0300

    This will add basic setup for gesture and on swipe enabled features.

---
+++ app/src/main/java/com/moringaschool/cookie/util/OnStartDragListener.java
@@ -0,0 +1,7 @@
+package com.moringaschool.cookie.util;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public interface OnStartDragListener {
+    void onStartDrag(RecyclerView.ViewHolder viewHolder);
+}
---
+++ app/src/main/java/com/moringaschool/cookie/util/SimpleItemTouchHelperCallback.java
@@ -0,0 +1,50 @@
+package com.moringaschool.cookie.util;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SimpleItemTouchHelperCallback extends ItemTouchHelper.Callback {
+    private final ItemTouchHelperAdapter mAdapter;
+
+
+    public SimpleItemTouchHelperCallback(ItemTouchHelperAdapter adapter) {
+        mAdapter = adapter;
+    }
+
+    @Override
+    public boolean isLongPressDragEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean isItemViewSwipeEnabled() {
+        return true;
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        final int dragFlags = ItemTouchHelper.UP | ItemTouchHelper.DOWN;
+        final int swipeFlags = ItemTouchHelper.START | ItemTouchHelper.END;
+        return makeMovementFlags(dragFlags, swipeFlags);
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, RecyclerView.ViewHolder source,
+                          RecyclerView.ViewHolder target) {
+        if (source.getItemViewType() != target.getItemViewType()) {
+            return false;
+        }
+        mAdapter.onItemMove(source.getAdapterPosition(), target.getAdapterPosition());
+        return true;
+    }
+
+
+    @Override
+    public void onSwiped(RecyclerView.ViewHolder viewHolder, int i) {
+        mAdapter.onItemDismiss(viewHolder.getAdapterPosition());
+    }
+
+
+}
+
